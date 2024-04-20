b22b39af5e1fe8efbbbd07a8b34d55e97793ce2d
ilyapc <ilya.komendantov@list.ru>
2024-04-20 07:58:44 +0300

    refactoring

--- app/src/main/java/ru/ikom/pictureeditor/MainActivity.kt
+++ app/src/main/java/ru/ikom/pictureeditor/MainActivity.kt
@@ -31,16 +31,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        if (Intent.ACTION_VIEW == intent.action || Intent.ACTION_EDIT == intent.action) {
-            if (intent.type?.startsWith("image/") == true) {
-                val imageUri = intent.data
-                if (imageUri != null) viewModel.openEditor(imageUri.toString())
-            }
+        clearCacheDir()
+        if (savedInstanceState == null) {
+            if (Intent.ACTION_VIEW == intent.action || Intent.ACTION_EDIT == intent.action) {
+                if (intent.type?.startsWith("image/") == true) {
+                    val imageUri = intent.data
+                    if (imageUri != null) viewModel.openEditor(imageUri.toString())
+                }
+            } else viewModel.openEditor()
         }
-        else viewModel.openEditor()
         lifecycleScope.launch {
             viewModel.screen().flowWithLifecycle(lifecycle, Lifecycle.State.RESUMED).collect {
                 it.show(supportFragmentManager, R.id.fragmentContainer)
+            }
+        }
+    }
+
+    private fun clearCacheDir() {
+        if (cacheDir.isDirectory) {
+            val files = cacheDir.listFiles()
+            files?.let {
+                for (file in it) {
+                    file.delete()
+                }
             }
         }
     }
--- features/editor/src/main/java/ru/ikom/editor/EditorViewModel.kt
+++ features/editor/src/main/java/ru/ikom/editor/EditorViewModel.kt
@@ -1,12 +1,9 @@
 package ru.ikom.editor
 
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 
 class EditorViewModel(
-    private val router: EditorRouter,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+    private val router: EditorRouter
 ) : ViewModel() {
     fun coup() = router.coup()
 }
